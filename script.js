7611ebd018a707fcc9e9bb41ddea0a0e94ae4e51
Jéssica Nascimento <77943618+jessicacfsb@users.noreply.github.com>
2021-11-29 19:07:21 -0300

    iniciando ToDo List

---
+++ script.js
@@ -0,0 +1,10 @@
+var texto = document.getElementById("texto-digitado")
+var botao = document.getElementById("botao-adicionar")
+
+function cadastrar() {
+    nome= "jessica",
+    idade= "25",
+    email= "jessica@gmail.com"
+}
+
+document.write(cadastrar)
