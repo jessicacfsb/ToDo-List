83a774fe9241e35a7e30df557815683894f40ff4
Jéssica Nascimento <77943618+jessicacfsb@users.noreply.github.com>
2021-11-30 20:14:58 -0300

    alteração 1

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 var texto = document.getElementById("texto-digitado")
 var botao = document.getElementById("botao-adicionar")
+var ul = document.getElementById("ul")
 
-function cadastrar() {
-    nome= "jessica",
-    idade= "25",
-    email= "jessica@gmail.com"
+function textoTotal() {
+    return texto.value.length  
 }
 
-document.write(cadastrar)
+function criarLista() {
+    var li = document.createElement("li")
+}
+function cadastrar() {
+    if (textoTotal() > 0 ){
+        criarLista()
+    }else{
+    window.alert("ERRO - Digite alguma atividade")
+    }   
+}
+
+function cadastrar_texto() {
+    if (textoTotal() > 0 && event.which === 13) {
+        criarLista()
+    }    
+}
+
+botao.addEventListener("click", cadastrar)
+texto.addEventListener("keypress", cadastrar_texto)
